e5f64d0072a8eca6e68b1f0a3e4a96f62afd96ce
MatheusFel33 <104394609+MatheusFelipe1988@users.noreply.github.com>
2024-05-18 10:35:16 -0400

    Update QuizService.jsx
    teste

--- quiz-online/utils/QuizService.jsx
+++ quiz-online/utils/QuizService.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const api = axios.create({
-	baseURL: "http://localhost:9192/api/quizzes"
+	baseURL: "https://quiz-online-six.vercel.app"
 })
 
 export const createQuestion = async(quizQustion) =>{
