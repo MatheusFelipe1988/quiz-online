67d0c45aba07d3e94ad2c6c301575dfee14d26d4
MatheusFel33 <104394609+MatheusFelipe1988@users.noreply.github.com>
2024-05-18 09:23:05 -0400

    Update Cors.java
    teste

--- quiz/src/main/java/com/onlie/quiz/config/Cors.java
+++ quiz/src/main/java/com/onlie/quiz/config/Cors.java
@@ -10,7 +10,7 @@ public class Cors implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:9192")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
         WebMvcConfigurer.super.addCorsMappings(registry);
     }
